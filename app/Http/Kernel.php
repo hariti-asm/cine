3bbd514a087b50c3860fdd5cd38c18e6ca6eac1a
Douaa <douaachemnane@gmail.com>
2024-02-27 22:26:31 +0100

    middlware finish

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -64,5 +64,7 @@ class Kernel extends HttpKernel
         'signed' => \App\Http\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+        'Member' => \App\Http\Middleware\MemberMiddleware::class,
+        'admin' => \App\Http\Middleware\AdminMiddleware::class,
     ];
 }
--- routes/web.php
+++ routes/web.php
@@ -23,29 +23,13 @@ use App\Http\Controllers\Auth\ProviderController;
 Route::get('/', function () {
     return view('auth/login');
 });
-Route::middleware(['auth'])->group(function () {
+Route::middleware(['auth', 'admin'])->group(function () {
+//e admin ici
+});
+Route::middleware(['auth' , 'Member'])->group(function () {
     Route::get('/Home', [MovieController::class, 'index'])->name('home');
     Route::get('/movie/{slug}', [MovieController::class, 'show'])->name('movie.show');
     Route::get('/hall/{id}', [SchemaController::class, 'show'])->name('schema.show');
-});
-Route::get('/', function () {
-    return view('auth/login');
-});
-Route::middleware(['auth'])->group(function () {
-    Route::get('/Home', [MovieController::class, 'index'])->name('home');
-    Route::get('/movie/{slug}', [MovieController::class, 'show'])->name('movie.show');
-    Route::get('/hall/{id}', [SchemaController::class, 'show'])->name('schema.show');
-});
-
-// Route::put('/seats/{id}', [SeatController::class, 'update'])->name('seats.update');
-
-
-
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
-Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
@@ -56,18 +40,15 @@ Route::middleware('auth')->group(function () {
 });
 
 
+
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
  
 Route::get('/auth/{provider}/redirect',[ProviderController::class,'redirect']);
 Route::get('/auth/{provider}/callback',[ProviderController::class,'callback']);
-
-Route::get('/logout', function () {
-    request()->session()->invalidate();
-    \Illuminate\Support\Facades\Auth::logout();
-    return redirect('/login');
-})->name('logout.home');
-
- 
-
 Route::get('/logout', function () {
     request()->session()->invalidate();
     \Illuminate\Support\Facades\Auth::logout();
